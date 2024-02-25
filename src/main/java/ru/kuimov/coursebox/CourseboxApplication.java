1150dede1217582fb0eb94080fd125fa5f835c0f
Kuimov Timofey <111085197+Leo13Gro@users.noreply.github.com>
2024-02-25 15:33:44 +0300

    Initial commit

---
+++ src/main/java/ru/kuimov/coursebox/CourseboxApplication.java
@@ -0,0 +1,13 @@
+package ru.kuimov.coursebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuimov/coursebox/CourseboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuimov.coursebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
